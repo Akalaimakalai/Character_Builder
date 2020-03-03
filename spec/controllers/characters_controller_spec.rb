2d34426592b076ed51c104116286881d62b47713
Ar4i <jackshaakara@gmail.com>
2020-03-04 00:39:27 +0300

    add 'DELETE #destroy' tests to character_controller_spec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -6,34 +6,34 @@ RSpec.describe CharactersController, type: :controller do
 
   describe 'GET #index' do
 
-  context 'Authenticated user' do
-    let(:characters) { create_list(:character, 3, user: user) }
-    let(:another_character) { create(:character) }
+    context 'Authenticated user' do
+      let(:characters) { create_list(:character, 3, user: user) }
+      let(:another_character) { create(:character) }
 
-    before do
-      login(user)
-      get :index
+      before do
+        login(user)
+        get :index
+      end
+
+      it "loads all user's characters into @characters" do
+        expect(assigns(:characters)).to eq characters
+      end
+
+      it "does not load other characters" do
+        expect(assigns(:characters)).to_not be_include(another_character)
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
     end
 
-    it "loads all user's characters into @characters" do
-      expect(assigns(:characters)).to eq characters
-    end
-
-    it "does not load other characters" do
-      expect(assigns(:characters)).to_not be_include(another_character)
-    end
-
-    it 'renders index view' do
-      expect(response).to render_template :index
+    context 'Unauthenticated user' do
+      it_behaves_like 'unauthenticated' do
+        before { get :index }
+      end
     end
   end
-
-  context 'Unauthenticated user' do
-    it_behaves_like 'unauthenticated' do
-      before { get :index }
-    end
-  end
-end
 
   describe 'GET #show' do
 
@@ -88,7 +88,7 @@ end
     context 'Authenticated user' do
       before { login(user) }
 
-      context ' creates character with valid params' do
+      context 'creates character with valid params' do
         it 'saves char in database' do
           expect { post :create, params: { character: { name: 'NewChar' } } }.to change(Character, :count).by(1)
         end
@@ -99,7 +99,7 @@ end
         end
       end
 
-      context ' creates character with invalid params' do
+      context 'creates character with invalid params' do
         it 'does not save char in database' do
           expect { post :create, params: { character: { name: '' } } }.to_not change(Character, :count)
         end
@@ -118,6 +118,51 @@ end
 
       it_behaves_like 'unauthenticated' do
         before { post :create, params: { character: { name: 'NewChar' } } }
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:character) { create(:character, user: user) }
+
+    context 'Authenticated user' do
+
+      context 'is the creator of this character' do
+        before { login(user) }
+
+        it 'deletes the character' do
+          expect { delete :destroy, params: { id: character } }.to change(Character, :count).by(-1)
+        end
+
+        it 'renders index view' do
+          delete :destroy, params: { id: character }
+          expect(response).to render_template :index
+        end
+      end
+
+      context 'is NOT the creator of this character' do
+        let(:user2){ create(:user) }
+
+        before { login(user2) }
+
+        it 'does not delete the character' do
+          expect { delete :destroy, params: { id: character } }.to_not change(Character, :count)
+        end
+
+        it 'renders index view' do
+          delete :destroy, params: { id: character }
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'Unauthenticated user' do
+      it 'does not delete the character' do
+        expect { delete :destroy, params: { id: character } }.to_not change(Character, :count)
+      end
+
+      it_behaves_like 'unauthenticated' do
+        before { delete :destroy, params: { id: character } }
       end
     end
   end
